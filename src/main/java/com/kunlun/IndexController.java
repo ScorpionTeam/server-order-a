2c66de5e4903b6c3abc0fb7a1af4aa5e79199c59
Jarven <769558579@qq.com>
2017-12-14 12:48:54 +0800

    业务划分

--- src/main/java/com/kunlun/IndexController.java
+++ src/main/java/com/kunlun/IndexController.java
@@ -1,9 +1,13 @@
 package com.kunlun;
 
+import com.kunlun.entity.Seller;
 import com.kunlun.result.BaseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -28,6 +32,13 @@ public class IndexController {
     public BaseResult hello1(@PathVariable String test) {
         System.out.println("传递的参数为：" + test);
         return BaseResult.success("参数传递成功");
+    }
+
+    @PostMapping("/hello/post")
+    public BaseResult post(@RequestBody Seller seller) {
+        System.out.println("POST请求进来");
+        System.out.println("打印参数：" + seller.toString());
+        return null;
     }
 
 }
